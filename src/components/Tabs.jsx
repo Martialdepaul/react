e4a458e57be5e5c66159877dde71cab39a3a71b2
Martialdepaul <bourangoneulomimartialdepaul@gmail.com>
2024-10-22 00:22:30 +0100

    Initial commit

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Tabs({ children, buttons, ButtonsContainer = "menu" }) {
+  // const ButtonsContainer = buttonsContainer;
+  return (
+    <>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
+      {children}
+    </>
+  );
+}
